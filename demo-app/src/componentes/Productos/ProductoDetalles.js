e35215c180344a9457e3edbd0e7f2274065e7284
racmamf16 <TMEID_2020@outlook.com>
2022-05-05 14:45:49 -0500

    Avances
    5 de Mayo 2022 2:45pm

--- demo-app/src/componentes/Productos/ProductoDetalles.js
+++ demo-app/src/componentes/Productos/ProductoDetalles.js
@@ -1,24 +1,39 @@
 import React, {useContext, useState, useEffect} from 'react'
 import{DataContext} from "../../context/Dataprovider";
 import {useParams} from "react-router-dom";
+import {ProductoItem} from "./ProductoItem";
 
 export const ProductoDetalles = () => {
     const value = useContext(DataContext)
     const [productos] = value.productos;
+    const addCarrito = value.addCarrito;
     const [detalle, setDetalle] = useState([])
     const [url, setUrl] = useState(0);
     const [images, setImages] = useState('')
     const params = useParams();
+    let item = 0;
 
     useEffect (() =>{
         productos.forEach(producto =>{
-
+        item = 0;
             if(producto.id == parseInt(params.id)){
             setDetalle(producto)
-            
+            setUrl(0)
             }
         })
     },[params.id, productos])
+
+   useEffect(( )=>{
+     const values = `${detalle.img1}${url}${detalle.img2}`
+     setImages(values);
+   },[url, params.id])
+
+    const handleInput = e =>{
+        const number = e.target.value.toString().padStart(2, '01')   
+          setUrl(number)   
+        console.log(number)
+    }
+    if(detalle.length < 1) return null;
   return (
     <>
         {
@@ -42,16 +57,41 @@ export const ProductoDetalles = () => {
                        <p>Tamaño</p>
                     </div>
                 </div>
-                <button>Añadir al carrito</button>
-                <img src= "https://stockx-360.imgix.net/Nike-LD-Waffle-Sacai-Black-Nylon/Images/Nike-LD-Waffle-Sacai-Black-Nylon/Lv2/img20.jpg?auto=format,compress&q=90&updated_at=1606321430&w=1000"alt={detalle.title} />
-                <input type="range" min="1" max="36" onChange={handleInput}/>
+                <button onClick={()=> addCarrito(detalle.id)}>Añadir al carrito</button>
+                {
+                    url ? <img src={images}alt={detalle.title} /> : <img src={detalle.image}alt={detalle.title} />
+                }
+                
+                <input type="range" min="1" max="36"  value={url} onChange={handleInput}/>
                 <div className="description" >
                 <p><b>Description: </b> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum necessitatibus soluta alias porro, saepe facere expedita asperiores quos fugit inventore ex, itaque sapiente quae pariatur beatae optio repellat aperiam quia possimus mollitia repellendus? Illo natus quam eaque impedit omnis pariatur!</p>
           <br/>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam vitae accusantium omnis, facere laudantium ipsa hic reprehenderit blanditiis quibusdam quos repellendus id illo reiciendis magni, aliquid beatae, consequatur sapiente! Sequi facere itaque,</p>
+          <br/>
+          <br/>
                 </div>
             </div>
         }
+
+        <h2 className="relacionado">Productos Relacionados</h2>
+        <div className="productos">
+      {productos.map((producto) =>{
+          if((item < 6 )&&(detalle.category === producto.category)){
+              item++;
+            return <ProductoItem 
+            key={producto.id} 
+            id={producto.id}
+            title={producto.title}
+            price={producto.price}
+            image={producto.image}
+            category={producto.category}
+            cantidad={producto.cantidad}
+            />
+          }
+        })
+    }
+         
+      </div>
     </>
   )
 }
